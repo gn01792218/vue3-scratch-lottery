f14d291e5de45d2fa01d118b50bf8b017d74740c
MSI\gn01792218 <gn01792218@hotmail.com>
2023-11-23 14:24:33 +0800

    [try] add test text

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <Header/>
+  <section class="text-3xl text-red-400">
+    <p>當前狀態: {{ status }}</p>
+    <p>擋掉預設動作 : {{ testText }}</p>
+  </section>
   <router-view/>
 </template>
 
@@ -12,22 +16,30 @@ document.addEventListener('wheel', function(event) {
 
 
 //防止手機端手指縮放網頁
+const status = ref("無狀態")
+const testText = ref("無動作")
 let lastTouchEnd = 0;
 document.addEventListener('touchstart', function(event) {
+  status.value = 'touchstart'
   if (event.touches.length > 1) {
     event.preventDefault()
+    testText.value = '多於一個手指，預防start動作'
   }
 })
 document.addEventListener('touchmove', function (event) {
+  status.value = 'touchmove'
   if (event.touches.length > 1) {
     event.preventDefault();
+    testText.value = '多於一個手指，預防move動作'
   }
 });
 
 document.addEventListener('touchend', function (event) {
+  status.value = 'touchEnd'
   var now = new Date().getTime();
   if (now - lastTouchEnd <= 300) {
     event.preventDefault();
+    testText.value = '手指離開的時間小於300毫秒，預防end動作'
   }
   lastTouchEnd = now;
 });
