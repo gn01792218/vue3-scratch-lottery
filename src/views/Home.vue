1e9c0bf5e2b7bde199c42b30eb7e105ce37ca236
MSI\gn01792218 <gn01792218@hotmail.com>
2023-11-23 13:43:56 +0800

    [feature and test] add user whell event preventDefault

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,14 @@
       <RouterLink to="/ScratcDemo">刮刮樂示範</RouterLink>
     </li>
   </ul>
+  <section class="bg-red-100 flex justify-center">
+    <div class="border-2 border-black w-[300px] h-[100px] overflow-scroll">
+      <p>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
+      <p>BBB</p>
+      <p>CCCCCCCC</p>
+      <p>縒我午我我五我擃我我我我我我我五我我我</p>
+    </div>
+  </section>
   <button type="button" @click="countFromRef++">count is: {{ countFromRef }}</button>
   <br>
   <div>
